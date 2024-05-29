96de6020a28972d7820d57a09d0274100b4cab4d
KORE <KORE@LAPTOP-FI1733H9>
2024-05-29 12:35:05 +0530

    Add project files.

---
+++ StaticPractical240524/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StaticPractical240524
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string example = "Racecar";
+
+            Console.WriteLine("Original: " + example);
+            Console.WriteLine("Reversed: " + StringUtils.Reverse(example));
+            Console.WriteLine("Uppercase: " + StringUtils.ToUpperCase(example));
+            Console.WriteLine("Is Palindrome: " + StringUtils.IsPalindrome(example));
+        }
+    }
+}
---
+++ StaticPractical240524/unity class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticPractical240524
+{
+    public static class StringUtils
+    {
+        public static string Reverse(string input)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+        public static string ToUpperCase(string input)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+
+            return input.ToUpper();
+        }
+        public static bool IsPalindrome(string input)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+
+            string reversed = Reverse(input);
+            return input.Equals(reversed, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
